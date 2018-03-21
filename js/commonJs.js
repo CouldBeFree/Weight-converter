3e5f8cd9c942747b5b0090c181139d48d2806465
Sanya Zoria <propokerstar90@gmail.com>
2018-03-21 13:54:05 +0200

    Upgraded app

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,6 +1,9 @@
+document.getElementById('output').style.visibility = 'hidden';
+
 document.getElementById('lbsInput').addEventListener('input', function (e) {
 
     var lbs = e.target.value;
+    document.getElementById('output').style.visibility = 'visible';
     document.getElementById('gramsOutput').innerHTML = lbs/0.002246;
     document.getElementById('kgOutput').innerHTML = lbs/2.2046;
     document.getElementById('ozOutput').innerHTML = lbs*16;
